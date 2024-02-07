60b8d1d320a6196c776d75ad4047e3f79c895639
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2024-02-07 15:56:26 -0300

    Add alt to picture

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.querySelector("#app").innerHTML = `
   <div>
     Should be error
-    <img src="https://images.freeimages.com/images/large-previews/35e/flat-coated-retriever-1339154.jpg?fmt=webp&h=350" />  
+    <img src="https://images.freeimages.com/images/large-previews/35e/flat-coated-retriever-1339154.jpg?fmt=webp&h=350" Alt="Dog Picture" />  
   </div>
 `;
