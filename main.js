a18f801cca55c056feb6fcaed08ec6266489ee78
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2024-02-07 12:18:11 -0300

    Add vanilla and workflow

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector("#app").innerHTML = `
+  <div>
+    <img src="https://images.freeimages.com/images/large-previews/35e/flat-coated-retriever-1339154.jpg?fmt=webp&h=350" />  
+  </div>
+`;
